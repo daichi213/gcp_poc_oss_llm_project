d239aa6d93786871f745d072f414135a44a7c304
daichi213 <bestabokadon910@gmail.com>
2025-08-16 13:09:45 +0900

    change the authentication from pswd to IAM User

--- google_cloud_infra/database.tf
+++ google_cloud_infra/database.tf
@@ -1,15 +1,3 @@
-variable "db_user" {
-  description = "The username for the Cloud SQL database."
-  type        = string
-  default     = "app-user"
-}
-
-variable "db_password" {
-  description = "The password for the Cloud SQL database user."
-  type        = string
-  sensitive   = true
-}
-
 resource "google_sql_database_instance" "postgres" {
   project             = var.project_id
   name                = "llm-app-db-instance"
@@ -34,6 +22,6 @@ resource "google_sql_database" "default" {
 resource "google_sql_user" "default" {
   project  = var.project_id
   instance = google_sql_database_instance.postgres.name
-  name     = var.db_user
-  password = var.db_password
+  name     = trimsuffix(google_service_account.gce_service_account.email, ".gserviceaccount.com")
+  type     = "IAM_USER"
 }
